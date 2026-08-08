671dd2a07c208afb3e61a9273a88c1d5f89126ea
KaustubhSingh24 <gigaks.singh@gmail.com>
2026-08-08 19:43:43 +0530

    First java code ready

---
+++ spiderchange.java
@@ -0,0 +1,13 @@
+class namchange {
+    public static void main(String[] args) {
+        System.out.println("Number swapping program:-");
+        int num1 = 10;
+        int num2 = 20;
+        int temp;
+        System.out.println("Before swapping: num1 = " + num1 + ", num2 = " + num2);
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("After swapping: num1 = " + num1 + ", num2 = " + num2);
+    }
+}
